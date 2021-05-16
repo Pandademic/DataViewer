7d9f5193ebbb5a3fbd86991092431bc058b0ece3
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-05-16 19:46:08 -0400

    added app skeleton
    ERRORS
    ___________
    #8
    #9
    import problems

---
+++ main.py
@@ -0,0 +1,12 @@
+#importing
+#import pandas as pd
+#import numpy as np
+import tkinter as tk
+#app settings
+app=tk.Tk()
+app.geometery="500x800"
+app.title("Data Viewer")
+#app
+
+#app run
+app.mainloop()
