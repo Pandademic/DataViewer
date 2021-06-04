c6fb8a2a071d6b26b06f9dd9bb1480090b5af5fc
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-06-04 09:45:19 -0400

    Update Main.py to read settings

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-# importing
-#import numpy as np
-#import pandas as pd
-# FIXME:ERR:#8,#9
+#importing
+import pandas as pd 
+import numpy as np
 from appJar import gui
 import statistics
 import random
 import os
 import sys
+import matplotlib as plt
+import yaml
+with open("settings.yaml", 'r') as stream:
+    try:
+        print(yaml.safe_load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
+#Functions
+
+#app initilization
+app = gui("Data Viewer","600x600")
+# app
+app.addLabel("Enter The data viewer Directory Path:")
+app.addDirectoryEntry("file")
+filename = app.getEntry("file")
+file = open(str(filename), "w")
+app.addLabel("Enter the file you want to read:")
+app.addFileEntry("f1")
+
+#app run
+app.go()
+
+
+
+
+
+
+
+#TODO:fix app icon
+
 # TODO: document all new dependencies
 # app settings
 app = gui("Data Viewer", "600x600")
